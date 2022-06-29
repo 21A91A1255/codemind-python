1c47d19663a26f34c2e149ba245376301504fc4f
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2022-06-29 20:12:59 +0530

    s=input() l=list(set(s)) s=list(s) if len(s)==len(l):     print("True") else:     print("False")

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,16 +1,7 @@
-n=[]
-n=input()
-r=len(n)
-k=0
-c=0
-for i in range(0,r):
-    for j in range(i+1,r):
-        k+=1
-        if(n[i]==n[j]):
-            c+=1
-#print(k)
-#print(c)
-if(c==0):
+s=input()
+l=list(set(s))
+s=list(s)
+if len(s)==len(l):
     print("True")
 else:
     print("False")
