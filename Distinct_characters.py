18d1ffb96bc372862f2a451ef17db9109ffbaf5e
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2022-07-14 19:47:01 +0530

    n=input() a=[] lower=set('abcdefghijklmnopqrstuvwxyz') for i in range(len(n)):     if n[i] in lower:         k=n.count(n[i])         if(k==1):             a.append(n[i]) print(''.join(sorted(a)))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,13 +1,10 @@
 n=input()
-a=0
-r=[]
+a=[]
 lower=set('abcdefghijklmnopqrstuvwxyz')
 for i in range(len(n)):
     if n[i] in lower:
         k=n.count(n[i])
-        if(k>=1):
-            a+=1
-            r.append(n[i])
-#print(r)
-t=set(r)
-print(''.join(sorted(t)))
+        if(k==1):
+            a.append(n[i])
+print(''.join(sorted(a)))
+        
